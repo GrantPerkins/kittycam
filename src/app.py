4da29c219e015c8716b3ff1a25e925fc12e65871
GrantPerkins <gcperk20@gmail.com>
2026-07-25 23:04:40 -0700

    client side fps

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ import logging
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
-from fastapi.responses import StreamingResponse
+from fastapi.responses import StreamingResponse, FileResponse
 
 from src.services.stream import KittyCamera
 
@@ -13,12 +13,19 @@ async def lifespan(app: FastAPI):
     yield
     kitty_cam.stop()
 
+
 app = FastAPI(lifespan=lifespan)
+
 kitty_cam = KittyCamera()
 logging.basicConfig(level=logging.INFO)
 
 
 @app.get("/")
+async def index():
+    return FileResponse("static/index.html")
+
+
+@app.get("/stream")
 async def stream():
     return StreamingResponse(
         kitty_cam.stream(),
