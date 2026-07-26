3d50f4dab4473afccab65eebc6ca30ccbea8fc39
GrantPerkins <gcperk20@gmail.com>
2026-07-25 21:43:51 -0700

    video stream

--- src/app.py
+++ src/app.py
@@ -1,12 +1,16 @@
 import logging
 from fastapi import FastAPI, Request
 
-from src.services.stream import hello_opencv
+from src.services.stream import get_camera, generate_frames
+from fastapi.responses import StreamingResponse
 
 app = FastAPI()
+camera = get_camera()
 logging.basicConfig(level=logging.INFO)
 
 @app.get("/")
-async def root(request: Request):
-    # img = hello_opencv()
-    return "hello world!"
+async def stream():
+    return StreamingResponse(
+        generate_frames(camera),
+        media_type="multipart/x-mixed-replace; boundary=frame"
+    )
--- src/services/stream.py
+++ src/services/stream.py
@@ -1,22 +1,30 @@
 import cv2
-import numpy as np
-import base64
 
-def hello_opencv():
-    # Create a black background image (300 height, 400 width, 3 color channels)
-    img = np.zeros((300, 400, 3), dtype=np.uint8)
+def get_camera():
+    camera = cv2.VideoCapture("/dev/video0")
+    return camera
 
-    # Add "Hello World" text to the image
-    cv2.putText(
-        img,
-        "Hello World",
-        (50, 150),
-        cv2.FONT_HERSHEY_SIMPLEX,
-        1,
-        (0, 255, 0),
-        2,
-        cv2.LINE_AA,
-    )
-    encoded_bytes = base64.b64encode(img)
-    return encoded_bytes
+def generate_frames(camera):
+    while True:
+        success, frame = camera.read()
+
+        if not success:
+            continue
+
+        ret, buffer = cv2.imencode(
+            ".jpg",
+            frame,
+            [cv2.IMWRITE_JPEG_QUALITY, 90]
+        )
+
+        if not ret:
+            continue
+
+        yield (
+            b"--frame\r\n"
+            b"Content-Type: image/jpeg\r\n\r\n" +
+            buffer.tobytes() +
+            b"\r\n"
+        )
+
 
